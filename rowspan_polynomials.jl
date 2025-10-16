fff290a9f7acf8fc8b4cee45f31812692363d4bb
noa-l-levi <93477602+noa-l-levi@users.noreply.github.com>
2025-10-16 16:27:48 +1100

    Fixed errors

--- rowspan_polynomials.jl
+++ rowspan_polynomials.jl
@@ -20,11 +20,9 @@ function rowspan_polynomials(reaction_network, complex_id_vec)
 
     M_a = vcat(M, e[complex_id_vec, :])
 
-    #Find the left nullspace of M_a 
-
     all_symbolics = unique(reduce(vcat, Symbolics.get_variables.(M_a)))
 
-    converted_Ma = symbolics_to_sympy.(M_a) #convert to SymPy matrix
+    converted_Ma = symbolics_to_sympy.(M_a)
 
     Sp_Ma = SymPy.Sym.(converted_Ma)
 
@@ -33,20 +31,11 @@ function rowspan_polynomials(reaction_network, complex_id_vec)
     converted_ns = sympy_to_symbolics.(Sp_ns, Ref(all_symbolics))
 
     NlMa = Num.(converted_ns)
+    
+    b = Symbolics.simplify.(-NlMa[1:n,(dm+1):end]) 
+    
+    invariants = Symbolics.simplify.(transpose(b)*M)
 
-    # NlMa = nullspace(transpose(M_a))
-
-    # setpoint = Symbolics.simplify(-NlMa[n+1,dm+1])
-    num_binoms = length(complex_id_vec) - 1
-    b = -NlMa[1:n,end-num_binoms+1:end]
-
-    binoms = Symbolics.simplify.(transpose(b)*M)
-    invariant = Symbolics.simplify.(sum(binoms, dims=1))
-     
-    if any(isequal.(invariant[complex_id_vec], 0))
-        @warn "No linear invariant exists involving ALL the given complexes"
-    end
-
-    return b, invariant
+    return b, invariants
     end
 end
